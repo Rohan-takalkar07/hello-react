0342263fa6839b0137df802cced6fb388242d3a7
rohan.takalkar <rtakalkar04@gmail.com>
2025-02-20 15:03:47 +0530

    two

--- src/App.js
+++ src/App.js
@@ -2,19 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
+
 function App() {
-  const [count,setCount]=useState(0);
-  function addcount(){
-  
-    setCount(count+1)
-  }
- 
+const[num,setNum]=useState("");
+function evenOdd(){
+if(num%2==0){
+  setNum(num+"is even")
+}
+else{
+setNum(num+"is odd")
+}
+}
 
   return (
     <div>
-      <h1>{count}</h1>
-      <button onClick={addcount}>addcount</button>
-      
+      <h1>{num}</h1>
+      <input type="number" onChange={(e)=>{setNum(e.target.value)}}/>
+      <button onClick={evenOdd}>check</button>
+
     </div>
   );
 }
